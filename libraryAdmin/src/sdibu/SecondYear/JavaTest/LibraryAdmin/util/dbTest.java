7dbae1e513cb12f23df3be520b0a1c964db73e32
bestsort <bestsort@DESKTOP-GU284LM>
2019-01-07 19:40:14 +0800

    all update

--- libraryAdmin/src/sdibu/SecondYear/JavaTest/LibraryAdmin/util/dbTest.java
+++ libraryAdmin/src/sdibu/SecondYear/JavaTest/LibraryAdmin/util/dbTest.java
@@ -2,7 +2,7 @@ package sdibu.SecondYear.JavaTest.LibraryAdmin.util;
 
 import java.sql.Connection;
 
-import sdibu.SecondYear.JavaTest.LibraryAdmin.bean.AdminUser;
+import sdibu.SecondYear.JavaTest.LibraryAdmin.bean.users;
 import sdibu.SecondYear.JavaTest.LibraryAdmin.dao.UserDaoImpl;
 import sdibu.SecondYear.JavaTest.LibraryAdmin.dao.UsersDao;
 
@@ -11,14 +11,18 @@ public class dbTest {
 
 	public static void main(String[] args) throws Exception {
 		//Connection  conn = DBTool.getConnection();
-		AdminUser user = new AdminUser("10005", "李二", "wasd51236", false, false);
+		/*users user = new users("10005", "李二", "wasd51236", false, false);
 		UserDaoImpl us = new UserDaoImpl();
-		us.addUser(user);//添加用户
+		us.addUser(user);//添加用户*/
 		
 		
 		/*UserDaoImpl us = new UserDaoImpl();
 		us.deleteUserById(10004);//删除用户*/
 		
+		
+		users user = new users("10006", "pony马", "123456", false, true);
+		UserDaoImpl us = new UserDaoImpl();
+		us.updateUser(user);//修改信息
 	}
 
 }
